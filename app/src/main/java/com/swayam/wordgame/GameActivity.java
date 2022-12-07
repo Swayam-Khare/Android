01289b13871380ce3a2be3f2f478e5cc059dbc5e
Swayam <swayamkhare78@gmail.com>
2022-12-07 20:46:15 +0530

    Button added in activity_game.xml, words.txt added, game logic updated

--- app/src/main/java/com/swayam/wordgame/GameActivity.java
+++ app/src/main/java/com/swayam/wordgame/GameActivity.java
@@ -3,12 +3,35 @@ package com.swayam.wordgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GameActivity extends AppCompatActivity {
+
+    String finalWord;
+    List<String> word_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        word_list = new ArrayList<>();
+        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.words));
+        while (scanner.hasNext()) {
+            word_list.add(scanner.nextLine());
+        }
+
+        startGame();
+    }
+
+    private void startGame() {
+        finalWord = word_list.get(new Random().nextInt(word_list.size()));
     }
 }
