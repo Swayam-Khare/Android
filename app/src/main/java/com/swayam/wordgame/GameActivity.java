06c6d87b3af1f7efef4262f648767a25a43e366e
swayam <swayamkhare78@gmail.com>
2023-05-31 00:21:32 +0530

    Added Alert Dialogs, minor fixes

--- app/src/main/java/com/swayam/wordgame/GameActivity.java
+++ app/src/main/java/com/swayam/wordgame/GameActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,6 +48,7 @@ public class GameActivity extends AppCompatActivity {
         while (scanner.hasNext()) {
             word_list.add(scanner.nextLine());
         }
+        scanner.close();
 
         Button submitButton = findViewById(R.id.submit_button);
         submitButton.setOnClickListener(v -> {
@@ -62,46 +66,71 @@ public class GameActivity extends AppCompatActivity {
         finalWord = word_list.get(new Random().nextInt(word_list.size())).toUpperCase();
         Log.i("GameActivity.class", "Answer: " + finalWord);
         inputField.setText("");
+
+        for (TextView wordView : wordLayout){
+            wordView.setText("");
+            wordView.setBackgroundColor(ContextCompat.getColor(this, R.color.statusBarColor));
+        }
     }
 
     private void checkGuess(String userGuess) {
+
+        if (userGuess.length() != 5){
+            Toast.makeText(this, "Enter a 5 letter guess", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         char user;
         char answer;
         numOfGuesses--;
+
         for (int i = 0; i < userGuess.length(); i++) {
             user = userGuess.charAt(i);
             answer = finalWord.charAt(i);
-            String guess = user + "";
-            wordLayout[i].setText(guess);
+            wordLayout[i].setText(user + "");
 
             if (user == answer) {
                 correctWords.add(user);
                 wordLayout[i].setBackgroundColor(ContextCompat.getColor(this, R.color.correct));
-
-            } else {
-                if (!correctWords.contains(user) && contains(finalWord, user)){
-                    wordLayout[i].setBackgroundColor(ContextCompat.getColor(this, R.color.half_correct));
-                }
-                else {
-                    wordLayout[i].setBackgroundColor(ContextCompat.getColor(this, R.color.incorrect));
-                }
+            }
+            else if (!correctWords.contains(user) && contains(finalWord, user)) {
+                wordLayout[i].setBackgroundColor(ContextCompat.getColor(this, R.color.half_correct));
+            }
+            else {
+                wordLayout[i].setBackgroundColor(ContextCompat.getColor(this, R.color.incorrect));
             }
         }
 
-        if (numOfGuesses < 1){
-            gameOver();
+        if (correctWords.size() == 5){
+            showAlertDialog("Correct!", "You guessed the word: ", "New Word");
+        }
+
+        if (numOfGuesses < 1) {
+            showAlertDialog("Nice Try!", "The word was: ", "Try Again");
         }
     }
 
-    private void gameOver() {
-        Intent intent = new Intent(this, OverActivity.class);
-        startActivity(intent);
-        finish();
+    private void showAlertDialog(String title, String message, String buttonText) {
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(this);
+        dialogBuilder
+                .setTitle(title)
+                .setCancelable(false)
+                .setMessage(message + finalWord)
+                .setNegativeButton(buttonText, (d, e) -> {
+                    startGame();
+                    d.dismiss();
+                })
+                .setPositiveButton("Exit", (d, e) -> {
+                    d.dismiss();
+                    finish();
+                });
+
+        dialogBuilder.create().show();
     }
 
     private boolean contains(String finalWord, char user) {
         for (int i = 0; i < finalWord.length(); i++) {
-            if (finalWord.charAt(i) == user){
+            if (finalWord.charAt(i) == user) {
                 return true;
             }
         }
